717dbc0a46aa9a6a0999906be96602cdf192a38d
kumarrau <RAUSHAN.KUMAR2@otis.COM>
2019-08-26 17:15:24 +0530

    list comprehension file consisting of main concepts.

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -3,6 +3,8 @@
 #    input sequence,
 #    a variable representing member of input sequence and
 #    an optional predicate part.
+from functools import reduce
+
 if __name__ == '__main__':
     ######################## squaring numbers in a given range if number is odd ##################
     lst = [x ** 2 for x in range(1, 11) if x % 2 == 1]
@@ -43,10 +45,42 @@ if __name__ == '__main__':
     print(lst * 2) # it justs populate list with a given number of times(2 in this case) within the same list
 
     ####################################################################################################
+
     list1 = [3, 4, 5]
     new_multiplied_list = [item * 2 for item in list1]
     print(new_multiplied_list)
     print("\n")
+
     listOfWords = ["this", "is", "python", "tutorial", "from", "intellipaat"]
     new_list = [word[0] for word in listOfWords]
     print(new_list)
+
+    #############################Map Functions in List Comprehensions ##########################################
+    letters = list(map(lambda x:x,new_list))
+    print(letters)
+
+    #################### Filter function in List Comprehensions #################################################
+    letters1 = list(filter(lambda x: x, new_list))
+    print(letters1)
+
+    #################### reduce function in List Comprehensions #################################################
+
+    new_list1 = reduce(lambda x, y: x+y, new_list)
+    print(new_list1)
+    list1 = [1, 2, 3, 4, 5, 6]
+    new_list = reduce(lambda x, y: x + y, list1)
+    print(new_list)
+
+    #################### Nested Lists in Python List Comprehensions #################################################
+    #
+    # for x in range(4, 7):
+    #     for y in range(1, 11):
+    #         print(f"{x} * {y} = {x*y}")
+
+    teble = [[x*y for x in range(4, 7) for y in range(1, 11)]]
+    print(teble)
+
+    ##############################################################################################################
+    lst = ['a', 'b', 'c', 'd', '', 'ef', 'fgh', '', '', 'qwerty', "", ""] # This function removes all the empty string/char from the list
+    lst = [item for item in lst if item]
+    print(lst, end=" ")
